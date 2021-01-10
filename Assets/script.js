2382ae1ecb345ede296d43a8a8c1b8e0e57104a2
Kris Pennimpede <kpennimpede@hotmail.com>
2021-01-10 12:01:45 -0500

    create editable timeblocks

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,9 @@
+// get todays date and format it
 var showToday = function () {
   var today = moment().format("[Today is] dddd MMMM Do YYYY");
   $("#currentDay").text(today);
 }
+
+
 
 showToday();
